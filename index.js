aaa92fe63f5f9498de66ff43a1fd2a628cd18686
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-26 00:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-kittens.push("Chermin-Meow");
+kittens.push('Ralph');
 
 kittens.shift("Kitty_hawkes");
