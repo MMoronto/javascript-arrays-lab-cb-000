52e6405b47ff115beebb95bba5e10a0fa8c74d90
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-26 00:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-function destructivelyPrependKitten(name) {
+function destructivelyRemoveLastKitten(name) {
   kittens.shift("Bob");
   return kittens
 }
