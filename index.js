012970dfed8bec276eb910715f2be848b23201db
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-26 00:10:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function des
-kittens.push('Ralph');
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return kittens
+}
 
-kittens.shift("Bob");
+function destructivelyAppendKitten(name) {
+  kittens.shift("Bob");
+  return kittens
+}
